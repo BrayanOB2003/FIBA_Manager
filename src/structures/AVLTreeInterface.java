5bd4a1d8b14a6866cee33946245f6d88399561b9
OrtizBrayan <brayanortizbastidas@gmail.com>
2021-10-31 14:20:22 -0500

    Added AVL interface

---
+++ src/structures/AVLTreeInterface.java
@@ -0,0 +1,11 @@
+package structures;
+
+public interface AVLTreeInterface<K extends Comparable<K>, V> {
+	public boolean add(K k, V v);
+	public boolean remove(K k);
+	public AVLNode<K,V> searchNode(K k);
+	public V search(K k);
+	public boolean isEmpty();
+	public int size();
+	public boolean keyExists(K k);
+}
